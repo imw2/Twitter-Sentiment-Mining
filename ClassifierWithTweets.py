835a664820f3f1703eb311539aa82bb62f696db5
DanielForgosh <44677473+DanielForgosh@users.noreply.github.com>
2018-12-10 17:06:40 -0500

    Add files via upload

--- ClassifierWithTweets.py
+++ ClassifierWithTweets.py
@@ -6,27 +6,36 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
-Stock_Data = pd.read_csv('Daily\\FB_Daily.csv',usecols=['Stock_Date','open','high',
-                                                  'low', 'close','volume'])
+Stock_Data = pd.read_csv('..\\faangStockTweets\\netflix.csv',usecols=['Stock_Date','open','high',
+                                                  'low', 'close','volume','Positive','Negative','Total','Tweet_Date'])
 def amount_change (row):
+    '''
     if ((row['close'] - row['open'])/row['open']) > .025:
         return 2
-    if ((row['4. close'] - row['1. open'])/row['open']) > .005:
+    if ((row['close'] - row['open'])/row['open']) > .005:
         return 1
-    if ((row['close'] - row['1. open'])/row['open']) > -.005:
+    if ((row['close'] - row['open'])/row['open']) > -.005:
         return 0
-    if ((row['close'] - row['1. open'])/row['open']) > -.025:
+    if ((row['close'] - row['open'])/row['open']) > -.025:
         return -1
-    if ((row['close'] - row['1. open'])/row['open']) < -.025:
+    if ((row['close'] - row['open'])/row['open']) < -.025:
         return -2
+    '''
+    if ((row['close'] - row['open'])/row['open']) > 0:
+        return 1
+    if ((row['close'] - row['open'])/row['open']) <= 0:
+        return 0
 
 Stock_Data['change'] = Stock_Data.apply(lambda row: amount_change (row),axis=1)
 
-print(Stock_Data)
+Stock_Data.to_csv(r'FinalNE1.csv',index=None,header=True)
 
-Stock_Data.to_csv(r'FinalFB1.csv',index=None,header=True)
 
-Stock_Prediction = pd.read_csv('FinalFB.csv')
+
+
+
+'''
+Stock_Prediction = pd.read_csv('FinalAM.csv')
 Stock_Prediction = pd.DataFrame(Stock_Prediction).head(30)
 print(Stock_Prediction)
 
@@ -45,3 +54,4 @@ y_pred = model.predict(X_test)
 
 print("Accuracy: ",metrics.accuracy_score(y_test,y_pred))
 metrics.confusion_matrix(y_test,y_pred)
+'''
